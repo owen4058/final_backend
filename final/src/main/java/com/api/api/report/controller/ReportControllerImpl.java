b8440be835d094ceb309eded39cf35a7ca69f9e0
ohjaechang <ertiop0708@naver.com>
2024-05-27 09:16:51 +0900

    report 수정

--- final/src/main/java/com/api/api/report/controller/ReportControllerImpl.java
+++ final/src/main/java/com/api/api/report/controller/ReportControllerImpl.java
@@ -7,24 +7,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import com.api.api.qna.Qna;
 import com.api.api.report.Report;
 import com.api.api.report.service.ReportService;
 
 
 @Controller("reportController")
-@RequestMapping("/admin/report")
+@RequestMapping("/report")
 public class ReportControllerImpl implements ReportController {
 
 	@Autowired
 	private ReportService reportService;
 	//1. 신고내용 db에 저장할 것 
-	@PostMapping("/report")
+	@PostMapping("")
     @Override
     public ResponseEntity<Report> saveReport(@RequestBody Report report) {
         int result = reportService.saveReport(report);
@@ -35,7 +32,7 @@ public class ReportControllerImpl implements ReportController {
         }
     }
 	//2. 관리자 페이지에 리스트 띄어주기
-	@GetMapping("/admin/report")
+	@GetMapping("/admin")
 	@Override
 	public ResponseEntity<List<Report>> getListReport() {
 		List<Report> reportList = reportService.getListReport();
@@ -46,7 +43,7 @@ public class ReportControllerImpl implements ReportController {
 		}
 	}
 	//3. 신고 제목+본문
-	@GetMapping("/admin/report/{id}")
+	@GetMapping("/admin/Report/{id}")
 	@Override
 	public ResponseEntity<Report> getDetail(@RequestParam int report_id) {
 		Report report = reportService.getDetail(report_id);
--- final/src/main/java/com/api/api/report/repository/ReportRepositoryImpl.java
+++ final/src/main/java/com/api/api/report/repository/ReportRepositoryImpl.java
@@ -6,7 +6,6 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.api.api.qna.Qna;
 import com.api.api.report.Report;
 
 @Repository("reportRepository")
